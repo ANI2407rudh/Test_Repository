ae297a93cf588d6ecf66b22ec2826279017bc5db
Anirudh Baranwal <40201694+ANI2407rudh@users.noreply.github.com>
2018-07-10 13:23:20 +0530

    Update Anir.sql

--- Anir.sql
+++ Anir.sql
@@ -1,4 +1,13 @@
-insert into xx_jen_table value ('B');
+Declare
+Var1 integer;
+Var2 integer;
+Var3 integer;
+Begin
+Var1:=&var1;
+Var2:=&var2;
+Var3:=var1+var2;
+Dbms_output.put_line(var3);
+End;
 show err;
 pause  Press ENTER to begin....
 exit;
