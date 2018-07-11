e83b765dfb295db2829af9d6855002ce7448a6a3
Anirudh Baranwal <40201694+ANI2407rudh@users.noreply.github.com>
2018-07-11 13:50:43 +0530

    Update Anir.sql

--- Anir.sql
+++ Anir.sql
@@ -1,11 +1,3 @@
-set serveroutput on;
-Declare
-Var1 integer;
-Var2 integer;
-Var3 integer;
-Begin
-Var1:=&var1;
-Var2:=&var2;
-Var3:=var1+var2;
-Dbms_output.put_line(var3);
-End;
+insert into xx_jen_table values ('C');
+pause  Press ENTER to begin....
+exit ;
