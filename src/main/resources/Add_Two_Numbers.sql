5e8b3994f485c780a6e2408d2dff3958c84f4a4d
ANI2407rudh <anirudh.b92@gmail.com>
2018-08-08 15:31:08 +0530

    VM kab milega

---
+++ src/main/resources/Add_Two_Numbers.sql
@@ -0,0 +1,9 @@
+Declare
+Var1 integer := 10;
+Var2 integer := 20;
+Var3 integer;
+Begin
+Var3:=var1+var2;
+Dbms_output.put_line(var3);
+End;
+/
--- src/main/resources/Hello_World.sql
+++ src/main/resources/Hello_World.sql
@@ -1,5 +1,5 @@
 DECLARE
-message1 varchar2(20):= 'Hello, IBM';
+message1 varchar2(20):= 'Hello, IBM1';
 BEGIN
 dbms_output.put_line(message1);
 END;
