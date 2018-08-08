cccbb9e06121d3799e945ab301ab8d1cbe90474b
ANI2407rudh <anirudh.b92@gmail.com>
2018-08-08 16:41:21 +0530

    ,nhjdjd

--- src/main/resources/Update_test_po.sql
+++ src/main/resources/Update_test_po.sql
@@ -1,6 +1,6 @@
 DECLARE
 v_org_code varchar2 (1000);
 BEGIN
-update test_po set type_lookup_code = 'STANDARD' where type_lookup_code = 'BLANKET';
+update test_po set type_lookup_code = 'BLANKET' where type_lookup_code = 'STANDARD';
 commit;
 END;
